0e937b6e144a6bdb78ba536f684a3a3f715516ce
resendecode <gabrielresende02@proton.me>
2024-03-12 15:49:18 +0100

    Commit inter inter

--- c_serveur/main.c
+++ c_serveur/main.c
@@ -1,9 +1,33 @@
 #include <stdlib.h>
+#include <errno.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <wait.h>
 #include <signal.h>
+#include <unistd.h>
 //TODO: finir les imports
 int main(void){
   //TODO: lancer autres processus, creer segment mémoire partagé, créer file méssages, fermer correctement à la fin
-  system("gnome-terminal");
-return 0;
+  //creation despace mémoire
+  char* adr1;
+  int shmid, cle = 5;
+    //ne pas oublier ipc_creat
+    if ((shmid = shmget(cle, sizeof(int), IPC_CREAT|0600)) == -1) {
+      printf("shm2.shmget: %s\n", strerror(errno));
+      exit(1);
+    }
+    if ((adr1 = (char *)shmat(shmid, NULL, 0)) == (char *)-1) {
+      printf("shm2.shmat: %s\n", strerror(errno));
+      exit(1);
+    }
+    if (shmdt(adr1) == -1) {
+      printf("shm2.shmdt: %s\n", strerror(errno));
+      exit(1);
+    }
+    //ne pas faire de shmctl car le segmnt est déjà supprimé dans le fils
+
+
+  return 0;
 }
---
+++ maven_chat/src/main/java/org/gabi/ClientRMI.java
@@ -0,0 +1,9 @@
+package org.gabi;
+import java.rmi.*;
+
+public class ClientRMI {
+  public static void main(String[] args) {
+    ICompte cmpt;
+
+  }
+}
