bdd57feffcd9d3cbcd440c03b0950273d46708e6
resendecode <gabrielresende02@proton.me>
2024-05-02 23:39:30 +0200

    petite correction erreur (ajout de boule infinie dans le processus gestion)

--- c_serveur/gestion.c
+++ c_serveur/gestion.c
@@ -9,9 +9,11 @@
 #define MAX_BUFFER_SIZE 200
 int gestion(int cg[2], int gc[2]){
   char buffer[MAX_BUFFER_SIZE];
-  memset(&buffer, 0, MAX_BUFFER_SIZE);
-  read(cg[0],&buffer, MAX_BUFFER_SIZE);
-  write(gc[1],"Gestionnn :p\n", MAX_BUFFER_SIZE);
-  printf("%s", buffer);
+  while (1){
+    memset(&buffer, 0, MAX_BUFFER_SIZE);
+    read(cg[0],&buffer, MAX_BUFFER_SIZE);
+    write(gc[1],"Gestionnn :p\n", MAX_BUFFER_SIZE);
+    printf("%s", buffer);
+  }
   return 0;
 }
